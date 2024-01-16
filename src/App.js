9a375794d1218993445a4bd7410c6e544a1d7a34
Martin Fojtek <martfojt@gmail.com>
2024-01-16 15:59:16 +0100

    Add readme content and comments for the code in app and components files

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
-import logo from './logo.svg';
+import React from 'react';
+import GenerateColor from './components';
 
 export default function App() {
+  // State for tracking the key used to re-render the GenerateColor component
+  const [key, setKey] = React.useState(0);
+
+  // Function to generate a new color
+  const generateNewColor = () => {
+    // Update the key to a new value to force re-render of GenerateColor
+    setKey((prevKey) => prevKey + 1);
+  };
+  // Style object for the button container
+  const buttonStyle = {
+    display: 'flex',
+    justifyContent: 'center',
+    marginTop: '20px',
+    marginLeft: '15px',
+    width: '300px',
+  };
+  // Style object for the main container
+  const container = {
+    width: '300px',
+  };
+
+  // Rendering the main app component
   return (
-    <div>
-      <input></input>
+    <div style={container}>
+      <GenerateColor key={`color-${key}`} />
+      <div style={buttonStyle}>
+        <button onClick={generateNewColor}>Generate</button>
+      </div>
     </div>
   );
 }
---
+++ src/components.js
@@ -0,0 +1,32 @@
+import randomColor from 'randomcolor';
+import React, { useEffect, useState } from 'react';
+
+export default function GenerateColor() {
+  // State for the current color
+  const [color, setColor] = useState('');
+
+  // Effect hook to generate a new color
+  useEffect(() => {
+    // Generate a random color and set it to state
+    const newColor = randomColor();
+    setColor(newColor);
+  }, []);
+
+  // Style object for the color display block
+  const blockStyle = {
+    backgroundColor: color,
+    color: 'white',
+    width: '300px',
+    height: '150px',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    fontWeight: 'bold',
+    fontSize: '20px',
+    border: '5px solid black',
+    margin: '10px',
+  };
+
+  // Rendering the color block with the generated color
+  return <div style={blockStyle}>Generated Color: {color}</div>;
+}
